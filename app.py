9742fb4467aa833ea3d09de9073f8a57094825f5
theandrew168 <steveday168@gmail.com>
2020-09-01 11:31:41 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+DATABASE = 'bloggulus.db'
+SECRET_KEY = 'da80d8393ca7bbaee75ffa7c4c09067a437485ccf421b1239ad7d7b52b8e6d56'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run()

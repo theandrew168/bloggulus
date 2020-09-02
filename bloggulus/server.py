0d0a60ef0835c409585cc37588dea2d0abd9233f
theandrew168 <steveday168@gmail.com>
2020-09-01 23:10:45 -0500

    Implement per-worker async client handling

--- bloggulus/server.py
+++ bloggulus/server.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 import io
 import multiprocessing as mp
 import os
+import selectors
 import socket
 import sys
 import traceback
@@ -10,6 +11,7 @@ from .app import app
 
 # References:
 # https://www.python.org/dev/peps/pep-3333/
+# https://ruslanspivak.com/lsbaws-part2/
 
 # os.environ['LISTEN_FDS'] will hold number of FDs
 #
@@ -21,16 +23,32 @@ from .app import app
 # SYSTEMD_SOCKET_FD_PORT_443 = 4
 
 # Benchmarks (hey http://bloggulus.com):
-# 1 process, serial - 4.66 RPS
+# single process, serial - 4.66 RPS
 # pre-forked (1 process), serial - 4.70 RPS
+# pre-forked (2 process), serial -
+# pre-forked (4 process), serial -
+# pre-forked (1 process), async -
+# pre-forked (2 process), async -
+# pre-forked (4 process), async -
 
 
-def handle_client(c, server_name, server_port):
-    req = c.recv(1024)
+def handle_client(mux, sock, mask):
+    req = sock.recv(1024)
+
+    # TODO: why does this ever occur?
+    if len(req) == 0:
+        mux.unregister(sock)
+        sock.close()
+        return
+
 #    print(req.decode())
 
-    req_lines = req.split(b'\r\n')
-    method, path, version = req_lines[0].decode().split()
+    try:
+        req_lines = req.split(b'\r\n')
+        method, path, version = req_lines[0].decode().split()
+    except:
+        print(req)
+        raise
 
     env = {}
 
@@ -46,8 +64,8 @@ def handle_client(c, server_name, server_port):
     # Required CGI vars
     env['REQUEST_METHOD']    = method
     env['PATH_INFO']         = path
-    env['SERVER_NAME']       = server_name
-    env['SERVER_PORT']       = server_port
+    env['SERVER_NAME']       = 'bloggulus.com'
+    env['SERVER_PORT']       = '80'
 
     resp_status = ''
     resp_headers = [
@@ -74,40 +92,65 @@ def handle_client(c, server_name, server_port):
 
 #    print(resp.decode())
 
-    c.sendall(resp)
+    sock.sendall(resp)
+    mux.unregister(sock)
+    sock.close()
+
+
+def accept_client(mux, sock, mask):
+    try:
+        c, _ = sock.accept()
+    except BlockingIOError:
+        # http://lkml.iu.edu/hypermail/linux/kernel/1508.0/01413.html
+        #
+        # New client connections will wake up all workers but
+        # only one will actually get the accept(). The others will
+        # all raise a blocking IO error (EGAIN under the hood?).
+        # An Linux-specific optimization would be to set
+        # EPOLLEXCLUSIVE on the underlying epoll object. This way,
+        # only a single process would get woken up to accept()
+        # the new client.
+        return
+
+    # the worker that _does_ accept() the client can move forward
+    c.setblocking(False)
+    mux.register(c, selectors.EVENT_READ, handle_client)
 
 
 def wsgi_worker(s):
-    host, port = s.getsockname()[:2]
-    server_name = socket.getfqdn(host)
-    server_port = str(port)
+    mux = selectors.DefaultSelector()
+    mux.register(s, selectors.EVENT_READ, accept_client)
 
-    while True:
-        try:
-            c, _ = s.accept()
-        except KeyboardInterrupt:
-            break
+    try:
+        while True:
+            events = mux.select()
+            for key, mask in events:
+                handler = key.data
 
-        try:
-            handle_client(c, server_name, server_port)
-        except:
-            traceback.print_exc()
-        finally:
-            c.close()
+                try:
+                    handler(mux, key.fileobj, mask)
+                except:
+                    # don't crash the worker if something goes wrong
+                    traceback.print_exc()
+                    mux.unregister(key.fileobj)
+                    key.fileobj.close()
+    except KeyboardInterrupt:
+        pass
 
 
 def runserver(s):
+    mp.set_start_method('spawn')
+
     workers = []
     for _ in range(os.cpu_count()):
         worker = mp.Process(target=wsgi_worker, args=(s,))
         worker.start()
-        print('started worker {}'.format(worker.pid))
-
         workers.append(worker)
+        print('started worker {}'.format(worker.pid))
 
     for worker in workers:
         try:
             worker.join()
-            print('stopped worker {}'.format(worker.pid))
         except KeyboardInterrupt:
             pass
+        print('stopped worker {}'.format(worker.pid))
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import multiprocessing as mp
 import os
 import socket
 
@@ -6,15 +5,16 @@ from bloggulus.server import runserver
 
 
 if __name__ == '__main__':
-    mp.set_start_method('spawn')
     if os.getenv('LISTEN_FDS'):
         s = socket.fromfd(3, socket.AF_INET, socket.SOCK_STREAM)
+        s.setblocking(False)
         print('Serving HTTP on port:', 80)
 
         runserver(s)
     else:
         addr = ('', 5000)
         s = socket.create_server(addr, backlog=128, reuse_port=True)
+        s.setblocking(False)
         print('Serving HTTP on port:', 5000)
 
         runserver(s)

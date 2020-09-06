97997518996ef9b473dadeed50f199ca71298b74
theandrew168 <steveday168@gmail.com>
2020-09-06 12:37:33 -0500

    Simplify main.py

--- main.py
+++ main.py
@@ -7,5 +7,4 @@ from bloggulus.app import app
 
 
 if __name__ == '__main__':
-    s = socket.create_server(('127.0.0.1', 8080), backlog=128, reuse_port=True)
-    serve(app, sockets=[s], threads=os.cpu_count() * 4)
+    serve(app, host='127.0.0.1', port=5000, threads=os.cpu_count() * 4)

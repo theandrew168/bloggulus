aba9188bd961f6d1ef2c9d5ac106b768ffcd091c
theandrew168 <steveday168@gmail.com>
2020-09-06 11:05:37 -0500

    Transition to Django

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
-import socket
 
 from waitress import serve
 
-from bloggulus.app import app
+from bloggulus.wsgi import application
 
 
 if __name__ == '__main__':
-    s = socket.create_server(('127.0.0.1', 8080), backlog=128, reuse_port=True)
-    serve(app, sockets=[s], threads=os.cpu_count() * 4)
+    serve(application, host='127.0.0.1', port=8000, threads=os.cpu_count() * 4)

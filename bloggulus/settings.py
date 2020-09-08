832e37276b2c98412a4d0171187622f5d9c96810
theandrew168 <steveday168@gmail.com>
2020-09-08 14:14:49 -0500

    Add migrate command to GHA. Good or bad?

--- bloggulus/settings.py
+++ bloggulus/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'bloggulus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'bloggulus' / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

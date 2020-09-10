a776f10de0cd1a31253cfcc35da46b12ba94001f
theandrew168 <steveday168@gmail.com>
2020-09-10 14:40:24 -0500

    Fix templates for nested apps

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_webserver',
-    'bloggulus.polls',
+    'bloggulus.polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [

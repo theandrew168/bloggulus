4835552c6bc891e119e0948d41cd0668001ebd0d
theandrew168 <steveday168@gmail.com>
2020-09-12 23:54:43 -0500

    Make many-to-many on User / Feed optional

--- bloggulus/feeds/models.py
+++ bloggulus/feeds/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Feed(models.Model):
-    users = models.ManyToManyField(User, related_name='feeds')
+    users = models.ManyToManyField(User, blank=True, related_name='feeds')
     title = models.CharField(max_length=200)
     url = models.URLField(unique=True)
     updated = models.DateTimeField()

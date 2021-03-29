373ecc9432ad4cd92723faf6ec62ef9db5ea5581
theandrew168 <steveday168@gmail.com>
2021-03-28 19:19:21 -0500

    Remove systemd socket handling

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ import (
 )
 
 func main() {
-	addr := flag.String("addr", "0.0.0.0:8080", "server listen address")
+	addr := flag.String("addr", "0.0.0.0:5000", "server listen address")
 	addblog := flag.Bool("addblog", false, "-addblog <feed_url> <site_url>")
 	syncblogs := flag.Bool("syncblogs", false, "sync blog posts with the database")
 	flag.Parse()
@@ -105,74 +105,6 @@ func main() {
 	mux.Handle("/register", http.HandlerFunc(app.HandleRegister))
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
-	// check if running via systemd
-	if os.Getenv("LISTEN_PID") == strconv.Itoa(os.Getpid()) {
-		if os.Getenv("LISTEN_FDS") != "2" {
-			log.Fatalf("expected 2 sockets from systemd, got %s\n", os.Getenv("LISTEN_FDS"))
-		}
-
-		// create http listener from the port 80 fd
-		s80 := os.NewFile(3, "s80")
-		httpListener, err := net.FileListener(s80)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// create http listener from the port 443 fd
-		s443 := os.NewFile(4, "s443")
-		httpsListener, err := net.FileListener(s443)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// redirect http to https
-		go http.Serve(httpListener, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			target := "https://" + r.Host + r.URL.Path
-			if len(r.URL.RawQuery) > 0 {
-				target += "?" + r.URL.RawQuery
-			}
-			http.Redirect(w, r, target, http.StatusMovedPermanently)
-		}))
-
-		// setup autocert to automatically obtain and renew TLS certs
-		m := &autocert.Manager{
-			Prompt:     autocert.AcceptTOS,
-			HostPolicy: autocert.HostWhitelist("bloggulus.com", "www.bloggulus.com"),
-		}
-
-		// create dir for autocert cache
-		dir := filepath.Join(os.Getenv("HOME"), ".cache", "golang-autocert")
-		if err := os.MkdirAll(dir, 0700); err != nil {
-			log.Printf("warning: autocert not using cache: %v", err)
-		} else {
-			m.Cache = autocert.DirCache(dir)
-		}
-
-		// kick off blog sync task
-		syncBlogs := tasks.SyncBlogs(blogStorage, postStorage)
-		go syncBlogs.Run(1 * time.Hour)
-
-		// kick off session prune task
-		pruneSessions := tasks.PruneSessions(sessionStorage)
-		go pruneSessions.Run(5 * time.Minute)
-
-		// set min version to TLS 1.2
-		tlsConfig := m.TLSConfig()
-		tlsConfig.MinVersion = tls.VersionTLS12
-
-		s := &http.Server{
-			Handler:      mux,
-			TLSConfig:    tlsConfig,
-			IdleTimeout:  time.Minute,
-			ReadTimeout:  5 * time.Second,
-			WriteTimeout: 10 * time.Second,
-		}
-
-		log.Println("Listening on 0.0.0.0:80 and 0.0.0.0:443")
-		s.ServeTLS(httpsListener, "", "")
-	} else {
-		// local development setup
-		log.Printf("Listening on %s\n", *addr)
-		log.Fatal(http.ListenAndServe(*addr, mux))
-	}
+	log.Printf("Listening on %s\n", *addr)
+	log.Fatal(http.ListenAndServe(*addr, mux))
 }

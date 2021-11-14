acb70baff49e2373f44e19b7f9c40a8fb096417f
theandrew168 <andrew@shallowbrooksoftware.com>
2021-11-14 09:52:15 -0600

    Add test for searching on the index page

--- internal/web/index_test.go
+++ internal/web/index_test.go
@@ -35,3 +35,29 @@ func TestHandleIndex(t *testing.T) {
 		t.Fatalf("want %v, got %v\n", 200, resp.StatusCode)
 	}
 }
+
+func TestHandleIndexSearch(t *testing.T) {
+	conn := test.ConnectDB(t)
+	defer conn.Close()
+
+	storage := postgresql.NewStorage(conn)
+	logger := test.NewLogger()
+
+	app := web.NewApplication(storage, logger)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/?q=foo", nil)
+
+	router := app.Router()
+	router.ServeHTTP(w, r)
+
+	resp := w.Result()
+	_, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Fatalf("want %v, got %v\n", 200, resp.StatusCode)
+	}
+}

4a7c9de1a65f55d479cff6a2aed66e93591192ca
theandrew168 <andrew@shallowbrooksoftware.com>
2021-12-01 22:20:13 -0600

    Sketch out DB helper funcs

---
+++ internal/postgresql/scan.go
@@ -0,0 +1,17 @@
+package postgresql
+
+import (
+
+)
+
+func scan(row pgx.Row, dest ...interface{}) error {
+	// TODO: pgerrcode.UniqueViolation -> core.ErrExist
+	// TODO: pgx.ErrNoRows -> core.ErrNotExist
+	// TODO: pgerrcode.AdminShutdown -> retry
+	//	but how? need to call Query[Row] again?
+	return nil
+}
+
+// TODO: instead write wrappers for?
+// 	QueryRow(ctx context.Context, sql string, args ...interface{}) Row
+// 	Query(ctx context.Context, sql string, args ...interface{}) (Rows, error)
--- main.go
+++ main.go
@@ -33,6 +33,7 @@ import (
 
 // TODO: helpers for common DB retries / error handling?
 // TODO: SQL query timeouts (build into helpers?)
+// TODO: graceful shutdown of tasks (sep binary here would split the deadlines)
 // TODO: updated tests for new features (internal/api, etc)
 
 //go:embed migrations

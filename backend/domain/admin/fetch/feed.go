cba51dfafe6d9612a83ec9707783285a1de72303
theandrew168 <andrew@shallowbrooksoftware.com>
2024-04-05 00:19:07 -0500

    Add mock feed fetcher

--- backend/domain/admin/fetch/feed.go
+++ backend/domain/admin/fetch/feed.go
@@ -1,5 +1,7 @@
 package fetch
 
+import "errors"
+
 type FetchFeedResponse struct {
 	Feed         string
 	ETag         string
@@ -8,4 +10,27 @@ type FetchFeedResponse struct {
 
 type FeedFetcher interface {
 	FetchFeed(url, etag, lastModified string) (FetchFeedResponse, error)
+}
+
+var _ FeedFetcher = (*MockFeedFetcher)(nil)
+
+type MockFeedFetcher struct {
+	feeds map[string]string
+}
+
+func NewMMockFeedFetcher(feeds map[string]string) *MockFeedFetcher {
+	f := MockFeedFetcher{feeds: feeds}
+	return &f
+}
+
+func (f *MockFeedFetcher) FetchFeed(url, etag, lastModified string) (FetchFeedResponse, error) {
+	feed, ok := f.feeds[url]
+	if !ok {
+		return FetchFeedResponse{}, errors.New("feed not found")
+	}
+
+	resp := FetchFeedResponse{
+		Feed: feed,
+	}
+	return resp, nil
 }

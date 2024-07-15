281daf3255624ab60a3d6762a7523dc88b5b9432
theandrew168 <andrew@shallowbrooksoftware.com>
2024-07-15 00:03:28 -0500

    Get started with React Router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const app = document.getElementById("app")!;
-const root = createRoot(app);
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <h1 className="sans-serif">Hello, world</h1>,
+	},
+]);
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
 root.render(
 	<React.StrictMode>
-		<h1 className="sans-serif">Hello, world</h1>
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 );

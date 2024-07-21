a49523734795570832de8c30702c8892f542cc30
theandrew168 <andrew@shallowbrooksoftware.com>
2024-07-21 00:37:54 -0500

    Show "Relevant" when searching

--- frontend/src/pages/IndexPage.tsx
+++ frontend/src/pages/IndexPage.tsx
@@ -39,7 +39,7 @@ export default function IndexPage() {
 	return (
 		<>
 			<div className="max-w-3xl mx-auto flex justify-start items-center my-6 px-6 md:px-0">
-				<h1 className="text-xl font-bold text-gray-700 md:text-2xl">Recent Articles</h1>
+				<h1 className="text-xl font-bold text-gray-700 md:text-2xl">{q ? "Relevant" : "Recent"} Articles</h1>
 			</div>
 
 			<div className="px-6 md:px-0">

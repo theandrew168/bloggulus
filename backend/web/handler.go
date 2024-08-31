16e6d7c244c4477fcfc12c1ec6f1f702fc7c89a0
theandrew168 <andrew@shallowbrooksoftware.com>
2024-08-31 13:05:04 -0500

    Improve some comments, add custom 404 page

--- backend/web/handler.go
+++ backend/web/handler.go
@@ -20,28 +20,36 @@ func Handler(
 ) http.Handler {
 	mux := http.NewServeMux()
 
-	// metrics
+	// Host prometheus metrics on "/metrics".
 	mux.Handle("GET /metrics", promhttp.Handler())
 
-	// basic health check
+	// Basic health check endpoint for verifying that the app is running.
 	mux.HandleFunc("GET /ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
 		w.Write([]byte("pong\n"))
 	})
 
-	// compress public files
+	// Compress and serve the embedded public (static) files.
 	publicFiles, _ := fs.Sub(public, "public")
 	publicHandler := gzhttp.GzipHandler(http.FileServer(http.FS(publicFiles)))
 
-	// serve non-index static files from the public FS
+	// Serve public (static) files from the embedded FS.
 	mux.Handle("/favicon.ico", publicHandler)
+	mux.Handle("/favicon.png", publicHandler)
 	mux.Handle("/robots.txt", publicHandler)
 	mux.Handle("/css/", publicHandler)
 	mux.Handle("/fonts/", publicHandler)
 
+	// The main application routes start here.
 	mux.Handle("/{$}", page.HandleIndex(store))
 	mux.Handle("GET /register", page.HandleRegister())
 	mux.Handle("POST /register", page.HandleRegisterForm(store))
+
+	// Requests that don't match any of the above handlers get a 404.
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+		w.Write([]byte("<h1>Bloggulus - Not Found</h1>"))
+	})
 
 	return middleware.Use(mux,
 		middleware.RecoverPanic(),

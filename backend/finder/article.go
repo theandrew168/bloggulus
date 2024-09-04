d2b88f4b79dbbf525515577928e8cde227139c88
theandrew168 <andrew@shallowbrooksoftware.com>
2024-09-04 18:04:11 -0500

    Limit articles to three tags

--- backend/finder/article.go
+++ backend/finder/article.go
@@ -34,10 +34,10 @@ func (f *Finder) ListArticles(limit, offset int) ([]Article, error) {
 		SELECT
 			post.title,
 			post.url,
-			blog.title as blog_title,
-			blog.site_url as blog_url,
+			MAX(blog.title) as blog_title,
+			MAX(blog.site_url) as blog_url,
 			post.published_at,
-			array_remove(array_agg(tag.name ORDER BY ts_rank_cd(post.fts_data, plainto_tsquery('english', tag.name)) DESC), NULL) as tags
+			(array_remove(array_agg(tag.name ORDER BY ts_rank_cd(post.fts_data, plainto_tsquery('english', tag.name)) DESC), NULL))[1:3] as tags
 		FROM latest
 		INNER JOIN post
 			ON post.id = latest.id
@@ -45,7 +45,7 @@ func (f *Finder) ListArticles(limit, offset int) ([]Article, error) {
 			ON blog.id = post.blog_id
 		LEFT JOIN tag
 			ON plainto_tsquery('english', tag.name) @@ post.fts_data
-		GROUP BY 1,2,3,4,5
+		GROUP BY post.id
 		ORDER BY post.published_at DESC`
 
 	ctx, cancel := context.WithTimeout(context.Background(), postgres.Timeout)
@@ -81,10 +81,10 @@ func (f *Finder) ListArticlesByAccount(account *model.Account, limit, offset int
 		SELECT
 			post.title,
 			post.url,
-			blog.title as blog_title,
-			blog.site_url as blog_url,
+			MAX(blog.title) as blog_title,
+			MAX(blog.site_url) as blog_url,
 			post.published_at,
-			array_remove(array_agg(tag.name ORDER BY ts_rank_cd(post.fts_data, plainto_tsquery('english', tag.name)) DESC), NULL) as tags
+			(array_remove(array_agg(tag.name ORDER BY ts_rank_cd(post.fts_data, plainto_tsquery('english', tag.name)) DESC), NULL))[1:3] as tags
 		FROM latest
 		INNER JOIN post
 			ON post.id = latest.id
@@ -92,7 +92,7 @@ func (f *Finder) ListArticlesByAccount(account *model.Account, limit, offset int
 			ON blog.id = post.blog_id
 		LEFT JOIN tag
 			ON plainto_tsquery('english', tag.name) @@ post.fts_data
-		GROUP BY 1,2,3,4,5
+		GROUP BY post.id
 		ORDER BY post.published_at DESC`
 
 	ctx, cancel := context.WithTimeout(context.Background(), postgres.Timeout)
@@ -123,10 +123,10 @@ func (f *Finder) SearchArticles(search string, limit, offset int) ([]Article, er
 		SELECT
 			post.title,
 			post.url,
-			blog.title as blog_title,
-			blog.site_url as blog_url,
+			MAX(blog.title) as blog_title,
+			MAX(blog.site_url) as blog_url,
 			post.published_at,
-			array_remove(array_agg(tag.name ORDER BY ts_rank_cd(post.fts_data, plainto_tsquery('english', tag.name)) DESC), NULL) as tags
+			(array_remove(array_agg(tag.name ORDER BY ts_rank_cd(post.fts_data, plainto_tsquery('english', tag.name)) DESC), NULL))[1:3] as tags
 		FROM relevant
 		INNER JOIN post
 			ON post.id = relevant.id
@@ -135,7 +135,7 @@ func (f *Finder) SearchArticles(search string, limit, offset int) ([]Article, er
 		LEFT JOIN tag
 			ON plainto_tsquery('english', tag.name) @@ post.fts_data
 		WHERE post.fts_data @@ websearch_to_tsquery('english',  $1)
-		GROUP BY 1,2,3,4,5,post.fts_data
+		GROUP BY post.id
 		ORDER BY ts_rank_cd(post.fts_data, websearch_to_tsquery('english',  $1)) DESC`
 
 	ctx, cancel := context.WithTimeout(context.Background(), postgres.Timeout)
@@ -171,10 +171,10 @@ func (f *Finder) SearchArticlesByAccount(account *model.Account, search string, 
 		SELECT
 			post.title,
 			post.url,
-			blog.title as blog_title,
-			blog.site_url as blog_url,
+			MAX(blog.title) as blog_title,
+			MAX(blog.site_url) as blog_url,
 			post.published_at,
-			array_remove(array_agg(tag.name ORDER BY ts_rank_cd(post.fts_data, plainto_tsquery('english', tag.name)) DESC), NULL) as tags
+			(array_remove(array_agg(tag.name ORDER BY ts_rank_cd(post.fts_data, plainto_tsquery('english', tag.name)) DESC), NULL))[1:3] as tags
 		FROM relevant
 		INNER JOIN post
 			ON post.id = relevant.id
@@ -183,7 +183,7 @@ func (f *Finder) SearchArticlesByAccount(account *model.Account, search string, 
 		LEFT JOIN tag
 			ON plainto_tsquery('english', tag.name) @@ post.fts_data
 		WHERE post.fts_data @@ websearch_to_tsquery('english',  $2)
-		GROUP BY 1,2,3,4,5,post.fts_data
+		GROUP BY post.id
 		ORDER BY ts_rank_cd(post.fts_data, websearch_to_tsquery('english',  $2)) DESC`
 
 	ctx, cancel := context.WithTimeout(context.Background(), postgres.Timeout)

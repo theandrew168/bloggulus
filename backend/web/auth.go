4dd213909229c0c0e8e635ac99afea676627b429
theandrew168 <andrew@shallowbrooksoftware.com>
2024-10-13 18:10:51 -0500

    Consolidate auth handlers

--- backend/web/auth.go
+++ backend/web/auth.go
@@ -309,3 +309,45 @@ func HandleDebugSignIn(repo *repository.Repository) http.Handler {
 		http.Redirect(w, r, next, http.StatusFound)
 	})
 }
+
+func HandleSignOutForm(repo *repository.Repository) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check for a session ID. If there isn't one, just redirect back home.
+		sessionID, err := r.Cookie(util.SessionCookieName)
+		if err != nil {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+
+		// Delete the existing session cookie.
+		cookie := util.NewExpiredCookie(util.SessionCookieName)
+		http.SetCookie(w, &cookie)
+
+		// Lookup the session by its client-side session ID.
+		session, err := repo.Session().ReadBySessionID(sessionID.Value)
+		if err != nil {
+			switch {
+			case errors.Is(err, postgres.ErrNotFound):
+				http.Redirect(w, r, "/", http.StatusSeeOther)
+			default:
+				util.InternalServerErrorResponse(w, r, err)
+			}
+			return
+		}
+
+		// Delete the session from the database.
+		err = repo.Session().Delete(session)
+		if err != nil {
+			switch {
+			case errors.Is(err, postgres.ErrNotFound):
+				http.Redirect(w, r, "/", http.StatusSeeOther)
+			default:
+				util.InternalServerErrorResponse(w, r, err)
+			}
+			return
+		}
+
+		// Redirect back to the index page.
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+}
